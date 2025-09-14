b0a98ecaaf827fc3f3413b3ea9df52a544c9d038
ConradOsawe <anthonyconrad93@gmail.com>
2025-09-14 22:51:21 +0530

    update WaitAndNotify

--- WaitAndNotify.java
+++ WaitAndNotify.java
@@ -1,5 +1,11 @@
 public class WaitAndNotify {
     public static void main(String[] args) {
+        Thread one = new Thread(() -> {
 
+        });
+
+        Thread two = new Thread(() -> {
+
+        });
     }
 }
