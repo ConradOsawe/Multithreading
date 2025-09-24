c0dd5d77d14b4114e30c9589fcfd6526791691a5
ConradOsawe <anthonyconrad93@gmail.com>
2025-09-24 23:23:14 +0530

    Update ProducerConsumer

--- ProducerConsumer.java
+++ ProducerConsumer.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class ProducerConsumer {
     class Worker {
 
+        private int sequence = 0;
+        private final Integer top;
+        private final List<Integer> container;
+        private final Object lock = new Object();
+
+        public Worker(Integer top, Integer bottom) {
+            this.top = top;
+            this.bottom = bottom;
+            this.container = new ArrayList<>();
+        }
+
+        public void produce() {
+
+        }
+
+        public void consumer() {
+
+        }
     }
 }
